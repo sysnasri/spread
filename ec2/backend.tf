70e0847784d121faad095b268144394f5d60fbf7
Alireza Nasri <gg.nasri@gmail.com>
2021-12-11 18:12:09 +0330

    Terraform Deleted

--- ec2/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-nasri-projects"
-    key            = "terraform-state-key"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-nasri-project-state"
-
-  }
-}
