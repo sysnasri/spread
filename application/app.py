ba1c2a647ebb4c3e9c73930549573df2afa78a90
Alireza Nasri <gg.nasri@gmail.com>
2021-12-11 18:39:54 +0330

    set style for Helloworld!

--- application/app.py
+++ application/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 
-    return "Hello World Spreadgroup team! My future work place"
+    return '<h1 style="color:blue;text-align:center">Hello World Spreadgroup team! My future work place</h1>'
 
 
 if __name__ == '__main__':
